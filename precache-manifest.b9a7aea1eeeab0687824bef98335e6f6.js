e7ad2df54862220da9cc49f1d58b791340a4427b
rylam206 <75106499+rylam206@users.noreply.github.com>
2022-08-10 23:24:46 -0700

    Updates

---
+++ precache-manifest.b9a7aea1eeeab0687824bef98335e6f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b64dcd30a8dda41d8295b5e96168f62",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "238c73e74faabe6ad2cd",
+    "url": "/chat/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "0d83aea165db8ba4eadc",
+    "url": "/chat/static/js/2.13e083c2.chunk.js"
+  },
+  {
+    "revision": "238c73e74faabe6ad2cd",
+    "url": "/chat/static/js/main.d15ce0e2.chunk.js"
+  },
+  {
+    "revision": "bea0acdc64656bd5506c",
+    "url": "/chat/static/js/runtime~main.1e6a5c90.js"
+  }
+]);
